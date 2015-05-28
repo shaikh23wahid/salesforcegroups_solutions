7082651add2e85a68147493030842a70f3f10e69
Wahid Shaikh <wshaikh@pureprofile.com>
2015-05-28 09:33:10 +0530

    files added
    files added

---
+++ app.js
@@ -0,0 +1,23 @@
+// app.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// index page
+app.get('/home', function(req, res) {
+    res.render('pages/index');
+});
+
+app.listen(8888);
+console.log('You are communicating to port 8080');
+
